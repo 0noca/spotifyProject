6ae5964fe1a86bc77b8060289199d47223588a1c
0noca <onocastream@outlook.com>
2022-04-04 00:00:08 +0300

    Update sidebar.tsx
    Playlist

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -19,6 +19,7 @@ import {
     MdPlaylistAdd,
     MdFavorite,
 } from 'react-icons/md'
+import Link from 'next/link'
 
 const navMenu = [
     {
@@ -51,10 +52,12 @@ const musicMenu = [
     },
 ]
 
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
+
 const Sidebar = () => {
     return (
         <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
-            <Box paddingY="20px">
+            <Box paddingY="20px" height="100%">
 
                 <Box width="120px" marginBottom="20px" paddingX="20px">
                     <NextImage src="/zerotwo.jpg" height={60} width={120}/>
@@ -77,9 +80,7 @@ const Sidebar = () => {
                     </List>
                 </Box>
 
-                <Divider color="gray.800"/>
-
-                <Box marginTop="20px">
+                <Box marginBottom="20px">
                     <List spacing={2}>
                      {musicMenu.map(menu => (
                         <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
@@ -93,6 +94,24 @@ const Sidebar = () => {
                             </LinkBox>
                         </ListItem>
                      ))}
+                    </List>
+                </Box>
+
+                <Divider color="gray.800"/>
+
+                <Box height="66%" overflowY="auto" paddingY="20px">
+                    <List spacing={2}>
+                        {playlists.map(playlist => ( 
+                            <ListItem paddingX="5px" key={playlist}>
+                                <LinkBox>
+                                    <NextLink href='/'>
+                                        <LinkOverlay>
+                                            {playlist}
+                                        </LinkOverlay>
+                                    </NextLink>
+                                </LinkBox>
+                            </ListItem>
+                        ))}
                     </List>
                 </Box>
 
