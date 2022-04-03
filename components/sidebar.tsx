985eb32db204d47afcc5bcb986877fe6e99bf217
0noca <onocastream@outlook.com>
2022-04-03 23:43:47 +0300

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import NextImage from 'next/image'
+import NextLink from 'next/link'
 
 import {
     Box,
@@ -37,6 +38,19 @@ const navMenu = [
     },
 ]
 
+const musicMenu = [
+    {
+        name: 'Create Playlist',
+        icon: MdPlaylistAdd,
+        route: '/',
+    },
+    {
+        name: 'Favorites',
+        icon: MdFavorite,
+        route: '/favorites',
+    },
+]
+
 const Sidebar = () => {
     return (
         <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
@@ -50,9 +64,35 @@ const Sidebar = () => {
                     <List spacing={2}>
                         {navMenu.map(menu => (
                             <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
-
+                                <LinkBox>
+                                  <NextLink href={menu.route} passHref>
+                                      <LinkOverlay>
+                                      <ListIcon as={menu.icon} color="white" marginRight="20px"/>
+                                      {menu.name}
+                                      </LinkOverlay>
+                                  </NextLink>
+                                </LinkBox>
                             </ListItem>
                         ))}
+                    </List>
+                </Box>
+
+                <Divider color="gray.800"/>
+
+                <Box marginTop="20px">
+                    <List spacing={2}>
+                     {musicMenu.map(menu => (
+                        <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                            <LinkBox>
+                                <NextLink href={menu.route} passHref>
+                                    <LinkOverlay>
+                                    <ListIcon as={menu.icon} color="white" marginRight="20px"/>
+                                    {menu.name}
+                                    </LinkOverlay>
+                                </NextLink>
+                            </LinkBox>
+                        </ListItem>
+                     ))}
                     </List>
                 </Box>
 
